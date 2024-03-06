5e185a1b2db01a17503165f84fffd2ff550a11b6
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-06 16:16:27 +0200

    delete scroll in opened menu, add close menu on ESC, add hover

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import NavBar from '../nav-bar/nav-bar';
-// import MobileMenu from '../mobile-menu/mobile-menu';
+import MobileMenu from '../mobile-menu/mobile-menu';
 
 export default function Header() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMenu = () => setShowMobileMenu(!showMobileMenu);
+
+  const closeMenuOnEsc = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setShowMobileMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleBodyScroll = () => {
+      document.body.style.overflowY = showMobileMenu ? 'hidden' : 'auto';
+    };
+
+    if (showMobileMenu) {
+      document.body.style.overflowY = 'hidden';
+      window.addEventListener('scroll', handleBodyScroll);
+      window.addEventListener('keydown', closeMenuOnEsc);
+    } else {
+      document.body.style.overflowY = 'auto';
+      window.removeEventListener('scroll', handleBodyScroll);
+      window.removeEventListener('keydown', closeMenuOnEsc);
+    }
+
+    return () => {
+      document.body.style.overflowY = 'auto';
+      window.removeEventListener('scroll', handleBodyScroll);
+      window.removeEventListener('keydown', closeMenuOnEsc);
+    };
+  }, [showMobileMenu]);
+
   return (
     <header className="absolute z-10 w-full ">
       <div className="mx-auto max-w-[480px] md:max-w-[768px] xl:max-w-[1440px] px-5 md:px-8 xl:px-[104px] pt-9 md:pt-7 flex justify-between items-center">
@@ -16,14 +50,14 @@ export default function Header() {
         </nav>
 
         <button
-          className="md:hidden uppercase tracking-widest font-normal text-sm text-white"
+          className="md:hidden uppercase tracking-widest font-normal text-sm text-white outline-transparent hover:underline transition-all"
           type="button"
+          onClick={toggleMenu}
         >
           Menu
         </button>
+        {showMobileMenu && <MobileMenu onClick={toggleMenu} />}
       </div>
-
-      {/* <MobileMenu /> */}
     </header>
   );
 }
--- src/components/mobile-menu/mobile-menu.tsx
+++ src/components/mobile-menu/mobile-menu.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import NavBar from '../nav-bar/nav-bar';
+import navs from '../../app/navs.json';
+import Link from 'next/link';
 
-export default function MobileMenu() {
+export default function MobileMenu({ onClick }: { onClick: () => void }) {
   return (
-    <div>
+    <div className="z-20 flex justify-center items-center fixed top-0 left-0 w-screen h-screen bg-[rgba(1, 10, 5, 0.75)] backdrop-blur-[50px]">
       <button
-        className="uppercase tracking-widest font-normal text-sm"
+        className="absolute top-[43px] right-[20px] uppercase tracking-widest text-white font-normal text-sm hover:underline transition-all"
         type="button"
+        onClick={onClick}
       >
         Close
       </button>
-      <NavBar />
+      <ul className="flex flex-col gap-6 gap-12 justify-center items-center">
+        {navs.map((item) => {
+          return (
+            <li key={item.id}>
+              <Link
+                href={item.path}
+                // to={item.path}
+                // spy={true}
+                // smooth={true}
+                // // offset={50}
+                // duration={500}
+                className="font-normal text-white text-lg leading-[1.21] tracking-widest hover:underline transition-all"
+                onClick={onClick}
+              >
+                {item.nav}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
