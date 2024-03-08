46d30bcca1cf96b402089493d5885e5d02e86d0b
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-03-08 21:29:23 +0200

    add head with favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import Hero from '@/sections/hero/hero';
 import Header from '@/components/header/header';
 import About from '@/sections/about/about';
 import Services from '@/sections/services/services';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <main className="mx-auto">
         <Hero />
