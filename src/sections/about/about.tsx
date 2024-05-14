aa71c283241ca382faa815eaa16ed8f2b5d11dde
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-05-14 21:11:10 +0300

    create json for hero, about sections

--- src/sections/about/about.tsx
+++ src/sections/about/about.tsx
@@ -1,45 +1,48 @@
 import React from 'react';
 
+import data from '../../data/about.json';
 import './about.modules.css';
 
 export default function About() {
+  const {
+    title,
+    titleBold,
+    firstTextBold,
+    firstText,
+    secondText,
+    secondTextBold,
+    thirdTextTop,
+    thirdTextMiddle,
+    thirdTextBottom,
+    fourthTextBold,
+    fourthText,
+  } = data;
   return (
     <section id="about" className="section-about">
       <div className="container-about py-14 md:py-16 xl:py-20 mx-auto max-w-[480px] md:max-w-[768px] xl:max-w-[1280px] px-5 md:px-8 xl:px-6 md:grid md:grid-cols-3 xl:grid-cols-4 md:grid-rows-[auto_auto_auto_auto] xl:grid-rows-[auto_auto_auto]">
         <h2 className=" text-white uppercase font-thin text-[40px] md:text-[67px] xl:text-[98px] leading-[1.4] md:leading-[1.21] tracking-[-.04em] mb-2 col-start-1 col-end-3">
-          who <span className="font-medium">we are</span>
+          {title} <span className="font-medium">{titleBold}</span>
         </h2>
 
         <div className="w-[180px] md:w-[220px] xl:w-[292px] col-start-3 col-end-4 row-start-1 row-end-4 row-start-1 row-end-4 xl:row-end-3 mb-10 md:mb-16 xl:mb-[72px]">
           <p className="w-[180px] md:w-[220px] xl:w-[292px] text-white font-extralight text-sm md:text-base xl:text-lg leading-[1.43] md:leading-5 xl:leading-[1.333] mb-5 md:mb-4 xl:mb-6">
-            <span className="font-normal">a team of enthusiasts</span> who are
-            fully committed to the mission of creating unforgettable and
-            extraordinary trips to the most beautiful parts of the Carpathians.
-            Our goal is not just to show you the natural wonders of the
-            mountains, but to provide you with a deep immersion in their magical
-            atmosphere.
+            <span className="font-normal">{firstTextBold}</span> {firstText}
           </p>
           <p className="w-[180px] md:w-[220px] xl:w-[292px] text-white font-extralight text-sm  md:text-base xl:text-lg leading-[1.43] md:leading-5 xl:leading-[1.333]">
-            <span className="font-normal">We believe</span> that nature has the
-            power to inspire, strengthen character and provide new perspectives.
-            Therefore, each of our tours is aimed at unlocking your potential,
-            enriching your spiritual world and creating unforgettable memories.
+            <span className="font-normal">{secondTextBold}</span> {secondText}
           </p>
         </div>
 
         <div className="w-[180px] md:w-[221px] xl:w-[296px] text-white uppercase font-normal text-sm md:text-base xl:text-lg leading-[1.43] md:leading-5 xl:leading-[1.333] ml-auto mb-10 md:mb-16 xl:mb-0 col-start-1 col-end-2 row-start-3 row-end-4 xl:col-start-4 xl:col-end-5 ">
-          <p>From vacationers </p>
-          <p className="text-right">to active travelers</p>
+          <p>{thirdTextTop}</p>
+          <p className="text-right">{thirdTextMiddle}</p>
           <p className="font-extralight lowercase tracking-[-.01em] md:tracking-[.02em]">
-            we have a tour for everyone.
+            {thirdTextBottom}
           </p>
         </div>
 
         <p className="xl:w-[605px] text-white font-extralight text-justify text-sm md:text-base xl:text-lg leading-[1.43] md:leading-5 xl:leading-[1.333] col-start-2 col-end-4 row-start-4 row-end-5  xl:col-start-1 xl:col-end-3 xl:row-start-3 xl:row-end-4">
-          <span className="font-normal">We use methods</span> that are
-          time-tested and proven. Our expert guides with in-depth knowledge of
-          the Carpathian landscapes lead you safely through the mysteries of
-          these mountains.
+          <span className="font-normal">{fourthTextBold}</span> {fourthText}
         </p>
       </div>
     </section>
--- src/sections/hero/hero.tsx
+++ src/sections/hero/hero.tsx
@@ -10,24 +10,26 @@ import RightFrameTab from '/public/assets/svg/right-frame-tab.svg';
 import LeftFrameDesk from '/public/assets/svg/left-frame-desk.svg';
 import RightFrameDesk from '/public/assets/svg/right-frame-desk.svg';
 
+import data from '../../data/hero.json';
 import './hero.modules.css';
 
 export default function Hero() {
+  const { title, titleBold, sloganLeft, sloganRight, sloganBottom, resorts, text, button } = data;
   return (
     <section className="section-hero">
       <div className="md:flex justify-between md:gap-12 mx-auto max-w-[480px] md:max-w-[768px] xl:max-w-[1280px] px-5 md:px-8 xl:px-6 pt-[105px] md:pt-[121px]  xl:pt-[130px] pb-14">
         <p className=" md:hidden ml-auto w-[118px] md:w-[230px]  xl:w-[305px] text-white font-thin uppercase text-right text-[37px] md:text-[67px] xl:text-[98px] tracking-[.04em] md:tracking-[.13em] xl:tracking-normal leading-[1.22] mb-6">
-          <span className="font-medium xl:mr-2">7</span>days
+          <span className="font-medium xl:mr-2">{sloganLeft}</span>{sloganRight}
           <span className="block font-light uppercase text-xs md:text-sm xl:text-base tracking-[.79em] md:tracking-[1.85em] xl:tracking-[2.28em] leading-tight xl:leading-[1.19] mt-[-7px] xl:mt-[-8.5px]">
-            journey
+            {sloganBottom}
           </span>
         </p>
         <div>
           <h1 className=" w-[280px] md:w-[426px] xl:w-[646px] text-white uppercase font-thin text-[40px] md:text-[67px] xl:md:text-[98px] leading-[1.4] md:leading-[1.21] tracking-[-.04em] mb-6 md:mb-[68px] xl:mb-[148px]">
-            <span className="font-medium">Uncover</span> Carpathian’s Secrets
+            <span className="font-medium">{titleBold}</span> {title}
           </h1>
           <p className="w-[157px] md:w-[262px] xl:w-[646px] text-white font-extralight text-[10px] md:text-[14px] xl:text-base leading-[1.6] md:leading-[1.143] md:treaking-[0.09em] mb-6 md:mb-0">
-            Hoverla / Yaremche / Zakarpattia / Vorokhta / Synevyr Lake / Bukovel
+            {resorts}
           </p>
         </div>
         <div className="md:w-[230px] xl:w-[305px]">
@@ -38,9 +40,7 @@ export default function Hero() {
             </span>
           </p>
           <p className="xl:w-[294px] text-white font-extralight text-justify text-sm md:text-base xl:text-lg leading-[1.43] md:leading-[1.25] xl:leading-[1.333] mb-6 md:mb-7">
-            We offer you unforgettable trips to the most beautiful parts of the
-            Carpathians. Enjoy stunning views, exciting expeditions, and the
-            best service!
+            {text}
           </p>
           <div className="relative cursor-pointer md:inline-block">
             <Link
@@ -52,7 +52,7 @@ export default function Hero() {
               className="cursor-pointer inline-block w-full md:w-[230px] xl:w-[293px] py-[2.5px] md:py-[14px] xl:py-[16px] text-white uppercase font-bold text-lg xl:text-[32px] text-center leading-[2.67] md:leading-[1.29] bg-white/10 md:bg-white/5 transition-colors hover:bg-white/20"
               aria-label="Go to Contacts Section"
             >
-              join now
+              {button}
             </Link>
             <LeftFrameMob
               className="absolute md:hidden bottom-0 left-0"
